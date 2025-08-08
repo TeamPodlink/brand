086fac693e2624f5e41b8f4d008768c7a1f6d6a7
Nathan Gathright <nathan@linktr.ee>
2025-08-08 16:20:27 -0500

    feat: initial Astro brand kit with grouped assets, copy actions, and GH Pages config

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://docs.astro.build/en/reference/configuration-reference/
+export default defineConfig({
+  site: 'https://brand.podlink.com',
+});
+
+
+
